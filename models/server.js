3a40433333582705d60fb038442a90ca36f2e022
Kevin <kalvarez@datec.com.bo>
2022-04-26 23:22:04 -0400

    AA

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server{
 
     constructor(){
         this.app = express();
-        this.port = process.env.port;
+        this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios'
         this.middlewares();
         this.routes();
